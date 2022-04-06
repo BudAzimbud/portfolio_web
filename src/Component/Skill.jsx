193a477577208339acf4d1fe74b7cfdb4676a8d0
azim <azim@wgs-id.com>
2022-04-06 16:12:39 +0700

    add style text align center in icon skill

--- src/Component/Skill.jsx
+++ src/Component/Skill.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Skill.css";
-import { DiReact } from "react-icons/di";
+import { DiReact, DiRedis } from "react-icons/di";
 import { SiNestjs, SiMongodb, SiDocker } from "react-icons/si";
 function Skill() {
   return (
@@ -8,28 +8,34 @@ function Skill() {
       <p className="title-page">Technology used</p>
       <div className="summary">
         <div className="row">
-          <div className="col">
+          <div className="col" style={{ textAlign: "center" }}>
             <h5>Reactjs</h5>
             <h1>
               <DiReact />
             </h1>
           </div>
-          <div className="col">
+          <div className="col" style={{ textAlign: "center" }}>
             <h5>Nestjs</h5>
             <h1>
               <SiNestjs />
             </h1>
           </div>
-          <div className="col">
+          <div className="col" style={{ textAlign: "center" }}>
             <h5>Mongodb</h5>
             <h1>
               <SiMongodb />
             </h1>
           </div>
-          <div className="col">
+          <div className="col" style={{ textAlign: "center" }}>
             <h5>Docker</h5>
             <h1>
               <SiDocker />
+            </h1>
+          </div>
+          <div className="col" style={{ textAlign: "center" }}>
+            <h5>Redis</h5>
+            <h1>
+              <DiRedis />
             </h1>
           </div>
         </div>
